fe11e974bdf06cadb3ae364153c4bb76606ca865
PeaceK <yannis.labidi666@gmail.com>
2023-03-16 13:37:35 +0100

    Att j'essaye de setup un truc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io;
+
 fn main() {
+    let mut input: String = String::new();
     println!("Hello, world!");
+    io::stdin().read_line(&mut input)
+        .expect("bad input");
+    println!("{}", input.trim_end());
 }
