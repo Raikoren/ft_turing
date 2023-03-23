7448c11aba2a26ceac7f1d7da663efc257fee9b0
PeaceK <yannis.labidi666@gmail.com>
2023-03-23 17:29:25 +0100

    fix crash lors d'un appel sans arguments

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,11 @@ mod description;
 
 fn main() -> Result<(), Box<dyn Error>>{
     let args: Vec<String> = env::args().collect();
-    if args[1] == "help" || args.len() != 3 {
-        print!("usage: ft_turing jsonfile input
+    if args.len() != 3 || args[1] == "help" {
+        print!("usage: ft_turing jsonfile input\n
 positional arguments:
-\tjsonfile\t\t\tjson description of the machine
-\tinput\t\t\t\tinput of the machine
+\tjsonfile\t\tjson description of the machine
+\tinput\t\t\tinput of the machine\n
 optional arguments:
 \thelp\t\t\tshow this help message and exit");
         return Ok(())
@@ -29,7 +29,7 @@ optional arguments:
 fn run_machine(desc: Description, mut input: Vec<char>) {
     let mut state = String::from(desc.initial);
     let mut head = 0;
-    while state != desc.finals[0] {
+    while !desc.finals.contains(&state) {
         let transition = desc.transitions.get(&state).unwrap().iter().find(|&t| t.read == input[head]).unwrap();
         state = transition.to_state.clone();
         input[head] = transition.write;
